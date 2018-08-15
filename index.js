96c389012a65469cf6b22c2bfa9796f84b386835
Yomaira Escano <yjescano@gmail.com>
2018-08-15 21:52:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const returnFirstTwoDrivers = function (drivers) {
 }
 
 const returnLastTwoDrivers = function (drivers) {
-  return drivers.slice(-1)
+  return drivers.slice(-1, -2)
 }
 
 // function returnFirstTwoDrivers(drivers) {
