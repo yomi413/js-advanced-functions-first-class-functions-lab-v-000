aa83d9dbb733d2638fe26aca6bfaffbd513d1781
Yomaira Escano <yjescano@gmail.com>
2018-08-15 21:19:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // Code your solution in this file!
-function returnFirstTwoDrivers() {
-  const myVar = function myFunc(drivers) {
-    return drivers.slice(0, 2);
-  }
-  return myVar;
+const myVar = function returnFirstTwoDrivers(drivers) {
+  return drivers.slice(0, 2)
 }
+
+// function returnFirstTwoDrivers() {
+//   const myVar = function myFunc(drivers) {
+//     return drivers.slice(0, 2);
+//   }
+//   return myVar;
+// }
 
 // const newDrivers = function returnFirstTwoDrivers(drivers) {
 //   return drivers
