eec42d33ab7e04f95c67a297d18660cdf8790a7d
Yomaira Escano <yjescano@gmail.com>
2018-08-16 00:44:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,24 +22,3 @@ const fareTripler = createFareMultiplier(3)
 function selectDifferentDrivers(drivers, returnDrivers) {
   return returnDrivers(drivers)
 }
-
-// function returnFirstTwoDrivers(drivers) {
-//   return drivers.slice(0, 2)
-// }
-//
-// const myfunc = () => {
-//
-// }
-//
-// console.log(myfunc.name)
-
-// function returnFirstTwoDrivers() {
-//   const myVar = function myFunc(drivers) {
-//     return drivers.slice(0, 2);
-//   }
-//   return myVar;
-// }
-
-// const newDrivers = function returnFirstTwoDrivers(drivers) {
-//   return drivers
-// }
